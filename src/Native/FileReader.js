b2aa49b09e59aa44e58585fcbe79219d368d9994
Simon Hampton <hotbelgo@gmail.com>
2015-12-05 15:50:48 +0100

    refactored native code

--- src/Native/FileReader.js
+++ src/Native/FileReader.js
@@ -12,25 +12,7 @@ Elm.Native.FileReader.make = function(localRuntime){
 
     var Task = Elm.Native.Task.make(localRuntime);
 
-    // getTextFile : String -> Task error String
-    var getTextFile = function(id){
-        var inputId = document.getElementById(id);
-        // specified field must be an <input type='file' ...>
-        // so it must exist and
-        // it must have a .files element
-        // if (!inputId || typeof inputId.files != 'object') {
-        //     return callback(Task.fail({ctor : 'IdNotFound'}))
-        // }
-        //
-        // var fileUpload = inputId.files[0];
-        // if (fileUpload)
-        //     reader.readAsText(fileUpload);
-        // else callback(Task.fail({ctor : 'NoFileSpecified'}));
-        return readAsTextFile(inputId.files[0]);
-    };
-
-    // readAsTextFile : Value -> Task error String
-    var readAsTextFile = function(fileObjectToRead){
+    function useReader(method, fileObjectToRead) {
         return Task.asyncFunction(function(callback){
             var reader = new FileReader();
 
@@ -46,61 +28,67 @@ Elm.Native.FileReader.make = function(localRuntime){
                 return callback(Task.fail({ctor : 'NoValidBlob'}))
             }
 
-            reader.readAsText(fileObjectToRead);
+            reader[method](fileObjectToRead);
         });
+    }
+
+    // readAsTextFile : Value -> Task error String
+    var readAsTextFile = function(fileObjectToRead){
+        return useReader("readAsText", fileObjectToRead);
     };
 
     // readAsArrayBuffer : Value -> Task error String
     var readAsArrayBuffer = function(fileObjectToRead){
-        return Task.asyncFunction(function(callback){
-            var reader = new FileReader();
-
-            reader.onload = function(evt) {
-                return callback(Task.succeed(evt.target.result))
-            };
-
-            reader.onerror = function() {
-                return callback(Task.fail({ctor : 'ReadFail'}));
-            };
-
-            // specified field must be an <input type='file' ...>
-            // so it must exist and
-            // it must have a .files element
-            if (!fileObjectToRead || !(fileObjectToRead instanceof Blob)) {
-                return callback(Task.fail({ctor : 'NoValidBlob'}))
-            }
-
-            reader.readAsArrayBuffer(fileObjectToRead);
-        });
+        return useReader("readAsArrayBuffer", fileObjectToRead);
+        // return Task.asyncFunction(function(callback){
+        //     var reader = new FileReader();
+        //
+        //     reader.onload = function(evt) {
+        //         return callback(Task.succeed(evt.target.result))
+        //     };
+        //
+        //     reader.onerror = function() {
+        //         return callback(Task.fail({ctor : 'ReadFail'}));
+        //     };
+        //
+        //     // specified field must be an <input type='file' ...>
+        //     // so it must exist and
+        //     // it must have a .files element
+        //     if (!fileObjectToRead || !(fileObjectToRead instanceof Blob)) {
+        //         return callback(Task.fail({ctor : 'NoValidBlob'}))
+        //     }
+        //
+        //     reader.readAsArrayBuffer(fileObjectToRead);
+        // });
     };
 
     // readAsDataUrl : Value -> Task error String
     var readAsDataUrl = function(fileObjectToRead){
-        return Task.asyncFunction(function(callback){
-            var reader = new FileReader();
-
-            reader.onload = function(evt) {
-                return callback(Task.succeed(evt.target.result))
-            };
-
-            reader.onerror = function() {
-                return callback(Task.fail({ctor : 'ReadFail'}));
-            };
-
-            // specified field must be an <input type='file' ...>
-            // so it must exist and
-            // it must have a .files element
-            if (!fileObjectToRead || !(fileObjectToRead instanceof Blob)) {
-                return callback(Task.fail({ctor : 'NoValidBlob'}))
-            }
-
-            reader.readAsDataURL(fileObjectToRead);
-        });
+        return useReader("readAsDataURL", fileObjectToRead);
+        // return Task.asyncFunction(function(callback){
+        //     var reader = new FileReader();
+        //
+        //     reader.onload = function(evt) {
+        //         return callback(Task.succeed(evt.target.result))
+        //     };
+        //
+        //     reader.onerror = function() {
+        //         return callback(Task.fail({ctor : 'ReadFail'}));
+        //     };
+        //
+        //     // specified field must be an <input type='file' ...>
+        //     // so it must exist and
+        //     // it must have a .files element
+        //     if (!fileObjectToRead || !(fileObjectToRead instanceof Blob)) {
+        //         return callback(Task.fail({ctor : 'NoValidBlob'}))
+        //     }
+        //
+        //     reader.readAsDataURL(fileObjectToRead);
+        // });
     };
 
 
     return {
-        getTextFile : getTextFile,
         readAsTextFile : readAsTextFile,
         readAsArrayBuffer : readAsArrayBuffer,
         readAsDataUrl: readAsDataUrl
