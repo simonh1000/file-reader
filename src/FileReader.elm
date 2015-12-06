8e6461af7041df5c438fe172076032a9a261eb65
Simon Hampton <hotbelgo@gmail.com>
2015-12-06 09:09:15 +0100

    Move decoders into FileReader

--- src/FileReader.elm
+++ src/FileReader.elm
@@ -2,11 +2,14 @@ module FileReader
     ( FileRef
     , FileContentArrayBuffer
     , FileContentDataUrl
+    , NativeFile
     , Error(..)
     , readAsTextFile
     , readAsArrayBuffer
     , readAsDataUrl
     , toString
+    , parseSelectedFiles
+    , parseDroppedFiles
     ) where
 
 {-| Elm bindings to HTML5 Reader API.
@@ -20,7 +23,10 @@ import Signal
 import Task exposing (Task)
 
 import Native.FileReader
-import Json.Decode exposing (Value)
+import Json.Decode as Json exposing
+    (Decoder, Value, (:=), andThen, at, oneOf, succeed,
+     object1, object2, object4, string, int, null, value)
+import MimeHelpers
 
 type alias FileRef = Value
 type alias FileContentArrayBuffer = Value
@@ -81,5 +87,71 @@ toString err =
     case err of
         ReadFail -> "File reading error"
         NoValidBlob -> "Blob was not valid"
-        -- IdNotFound -> "Id Not Found"
-        -- NoFileSpecified -> "No file specified"
+
+
+{-| Helper type for the File JS event object resulting
+from file select and drag 'n drop. The first three elements are
+useful meta data, while the fourth is the handle needed to read
+the file
+
+    type alias NativeFile =
+        { name : String
+        , size : Int
+        , mimeType : Maybe MimeHelpers.MimeType
+        , blob : Value
+        }
+-}
+type alias NativeFile =
+    { name : String
+    , size : Int
+    , mimeType : Maybe MimeHelpers.MimeType
+    , blob : FileRef
+    }
+{-|
+-}
+parseSelectedFiles : Decoder (List NativeFile)
+parseSelectedFiles =
+    eventParser "target"
+
+{-|
+-}
+parseDroppedFiles : Decoder (List NativeFile)
+parseDroppedFiles =
+    eventParser "dataTransfer"
+
+-- Unexported Helpers
+-- Json decoders for the somewhat weird drop eventdata structure. the .dataTransfer.files property is a JS FileList object which is not an array so cannot
+-- be parsed with array, but has to be accessed in the form .dataTransfer.files[index]. This hopefully explains the strange (toString index)
+-- way to get a file at an index
+eventParser : String -> Decoder (List NativeFile)
+eventParser field =
+    at
+        [ field, "files" ]
+        (filesLength `andThen` parseFiles)
+
+filesLength : Decoder Int
+filesLength =
+    oneOf
+        [ object1 identity ("length" := int)
+        , null 0
+        ]
+
+parseFiles : Int -> Decoder (List NativeFile)
+parseFiles count =
+    List.foldl
+        (\e -> object2 (::) (fileAt e))
+        (succeed [])
+        [0 .. count - 1]
+
+fileAt : Int -> Decoder NativeFile
+fileAt index =
+    (Basics.toString index) := nativeFile
+
+nativeFile : Decoder NativeFile
+nativeFile =
+    object4
+        NativeFile
+            ("name" := string) -- name
+            ("size" := int) -- size
+            (object1 MimeHelpers.parseMimeType ("type" := string)) -- mime type that is parsed as string and then converted to a MimeType
+            value -- the whole JS File object as a Json.Value so we can pass it to a library that reads the content with a native FileReader
